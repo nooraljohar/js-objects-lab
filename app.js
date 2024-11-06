3f20f7f7b6b82d9acfbe32b87462d661cc46d1a9
nooraljohar <nooraljohar.a72@gmail.com>
2024-11-06 23:55:57 +0300

    third hw

--- app.js
+++ app.js
@@ -20,3 +20,57 @@ const game = {
 
   //Exercise 1
   console.dir(pokemon, { maxArrayLength: null })
+
+
+
+
+  //Exercise 2  
+  console.log(game)
+
+  //Exercise 3
+  game.difficulty =["easy","med","hard"];
+
+  //Exercise 4
+  game.party.push(pokemon[6])
+  console.log(game.party)
+
+  //Exercise 5
+  game.party.push(pokemon[8])
+  game.party.push(pokemon[78])
+  game.party.push(pokemon[112])
+  console.log(game.party)
+
+  //Exercise 6
+  game.gyms.forEach(gym => {
+    if (gym.difficulty < 3) {
+      gym.completed = true;
+    }
+  })
+  console.log(game.gyms)
+
+  //Exercise 7
+  game.party.splice(0,1,pokemon[7])
+  console.log(game.party)
+
+  //Exercise 8
+  game.party.forEach(party => {
+    console.log(party.name)
+  })
+  
+  //Exercise 9
+
+  pokemon.forEach(poke => {
+    if(poke.starter = true){
+       console.log(poke.name) //to print all the information about starter pokemon
+       console.log(poke.name) //to print only the name of the starter pokemon 
+    }
+  })
+
+  //Exercise 10
+  
+  game.catchPokemon = catchPokemon (pokemonObj) => {
+    party.push(pokemonObj);  // Add the pokemon to the party array
+  }
+
+  game.catchPokemon(pokemon[0]); 
+  console.log(game.party);
